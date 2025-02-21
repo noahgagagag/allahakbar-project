d68388262a39365eab77d700e3c33b7e04834e02
noahgagagag <noah.lagast@go-eureka.be>
2025-02-21 14:36:09 +0100

    main.blocks, main.ts bijwerken

--- main.ts
+++ main.ts
@@ -1,4 +1,4 @@
 basic.forever(function () {
     maqueen.MotorRun(maqueen.aMotors.M1, maqueen.Dir.CW, 134)
-    maqueen.MotorRun(maqueen.aMotors.M2, maqueen.Dir.CW, 136)
+    maqueen.MotorRun(maqueen.aMotors.M2, maqueen.Dir.CW, 134)
 })
